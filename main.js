99b600c9433bd01633570e83b7080ee0aaf123d4
Brian Vo <Vobrian025@gmail.com>
2023-05-04 18:30:29 -0400

    damn

--- main.js
+++ main.js
@@ -1,48 +1,41 @@
-const weather = async (city) => {
-    e.preventDefault();
-    const weatherInfo = e.target.id.value;
+const container = document.querySelector('.container')
 
-    // Dont know if I need these yet
-    // const max = e.target.main.temp_max;
-    // const min = e.target.main.temp_min;
-    // const humidity = e.target.main.humidity;
-    // const forecast = e.target.weather.description;
-
-    //url needs to be changed to find certain things if not working
-    const url = (`https://openweathermap.org/current?/${id}`);
-
-    const res = await fetch(url)
-    const data = await res.json();
-
-    render(data)
+ async function getWeather(city_name) {
+    
+    const ApiKey = `313afe2acdc11ee8aa3dc2483da459dd`
+    const url = (`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${ApiKey}`);
+    try {
+      const res = await axios.get(url)
+      console.log(res.data)
+      showWeather(res.data)
+    } catch(error) {
+      console.log(error)
+    }
 };
 
-const render = (data) => {
-    container.innerHTML = ' '
-    let newHtml;
-    if(data) {
-        const weather = data
-        newHtml = document.createElement('div')
-        newHtml.innerHtml = `
-        <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">LOCATION ${id}</h5>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">FORECAST ${weather.description}</li>
-    <li class="list-group-item">MAX TEMP ${main.temp_max}</li>
-    <li class="list-group-item">MIN TEMP ${main.temp_min}</li>
-    <li class="list-group-item">HUMIDITY ${main.humidity}</li>
-  </ul>
-</div>
-`
-    } else {
-        newHtml = document.createElement('h2')
-        newHtml.innerText = 'That is not a city'
-    }
-    container.append(newHtml)
-}
+function showWeather(data) {
+    console.log("HERE I AM")
+          
+    let forecast = data.weather[0].description
+    let max = data.main.temp_max
+    let min = data.main.temp_min
+    let humidity = data.main.humidity
+
+    console.log(forecast, max, min, humidity)
+
+    document.getElementById('forcast').innerHtml=forecast
+    document.getElementById('max').innerHtml=max
+    document.getElementById('min').innerHtml=min 
+    document.getElementById('humidity').innerHtml=humidity
+   }
+
+
 
 const form = document.querySelector('.searchBar');
-form.addEventListener('submit', weather)
+form.addEventListener('submit', (e) => {
+  console.log("HERE I AM ONCE AGAIN")
+  e.preventDefault()
+  let city_name = document.getElementById('city_name')
+  console.log(city_name.value)
+  getWeather(city_name.value)
+});
